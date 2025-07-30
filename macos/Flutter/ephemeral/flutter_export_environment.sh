e053392b8846288ac8fc27f4754c84516ea29e10
khalid <palahuddinalkhalid@gmail.com>
2025-07-30 19:52:26 +0800

    Renamed project to my_flutter_portfolio and updated app labels

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.29.2/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xyz/portfolio_app"
+export "FLUTTER_APPLICATION_PATH=/Users/xyz/my_flutter_portfolio"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
